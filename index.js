a73674c83bcd60303741bfc991e465e8fc58d37f
Unnati-96 <Unnati@sevaro.com>
2025-01-22 04:59:06 +0530

    created Testing Route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import testRouter from "./routes/crud.route.js"
 const app = express();
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
+app.use('/task',testRouter);
